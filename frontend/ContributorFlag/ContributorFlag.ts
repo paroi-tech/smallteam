59a2d5283a1804ff4c3c4c17382d88f20bc93d4e
kitihounel <kitihounel@gmail.com>
2017-10-06 13:07:40 +0100

    Added ContributorFlag

--- frontend/ContributorFlag/ContributorFlag.ts
+++ frontend/ContributorFlag/ContributorFlag.ts
@@ -1,3 +1,31 @@
-export default class ContributorFlag {
+import App from "../App/App"
+import { Dash, Bkb } from "bkb"
+import { Model, ContributorModel } from "../AppModel/AppModel"
+import { UpdateModelEvent } from "../AppModel/ModelEngine"
+import { render } from "monkberry"
 
+import * as template from "./contributorflag.monk"
+
+export default class ContributorFlag {
+  readonly el: HTMLElement
+
+  private view: MonkberryView
+
+  private model: Model
+
+  constructor(private dash: Dash<App>, readonly contributor: ContributorModel) {
+    this.model = this.dash.app.model
+
+    this.view = render(template, document.createElement("div"))
+    this.el = this.view.nodes[0] as HTMLElement
+    this.el.textContent = this.contributor.login.charAt(0).toLocaleUpperCase()
+    this.el.title = this.contributor.login
+
+    this.dash.listenTo<UpdateModelEvent>(this.model, "updateContributor").onData(data => {
+      let contributor = data.model as ContributorModel
+      if (contributor.id === this.contributor.id) {
+        // TODO: Update contributor flag
+      }
+    })
+  }
 }
