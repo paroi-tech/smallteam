f85e2ba74a2ed2cb620d240bd3d849a792306836
kitihounel <kitihounel@gmail.com>
2017-12-13 20:10:02 +0100

    Test use of properties in components

--- frontend/semantics/tasks/TaskAttachmentManager/TaskAttachmentManager.ts
+++ frontend/semantics/tasks/TaskAttachmentManager/TaskAttachmentManager.ts
@@ -7,7 +7,30 @@ const template = require("./TaskAttachmentManager.monk")
 const itemTemplate = require("./item.monk")
 
 export default class TaskAttachmentManager {
-  constructor(private dash: Dash) {
+  readonly el: HTMLElement
 
+  private model: Model
+  private currentTask: TaskModel | undefined
+
+  private view: MonkberryView
+
+  constructor(private dash: Dash) {
+    this.model = this.dash.app.model
+  }
+
+  private createView() {
+    this.view = render(template, document.createElement("div"))
+
+    let el = this.view.nodes[0] as HTMLElement
+
+    return el
+  }
+
+  get task(): TaskModel | undefined {
+    return this.currentTask
+  }
+
+  set task(task: TaskModel | undefined) {
+    this.currentTask = task
   }
 }
