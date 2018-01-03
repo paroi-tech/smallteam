a530f313f3b4823bb7a12ff6e665415294ae34a9
kitihounel <kitihounel@gmail.com>
2018-01-03 13:37:05 +0100

    Use contributor dialog in TaskForm

---
+++ frontend/semantics/contributors/ContributorSelectionDialog/ContributorSelectionDialog.ts
@@ -0,0 +1,71 @@
+import { Dash } from "bkb"
+import { Model, TaskModel, UpdateModelEvent, ContributorModel } from "../../../AppModel/AppModel"
+import App from "../../../App/App"
+import CheckboxMultiSelect from "../../../generics/CheckboxMultiSelect/CheckboxMultiSelect"
+import { render } from "monkberry"
+import ContributorBox from "../ContributorBox/ContributorBox"
+
+const template = require("./ContributorSelectionDialog.monk")
+
+export default class ContributorSelectionDialog {
+  readonly el: HTMLDialogElement
+  private buttonEl: HTMLButtonElement
+  private selectorContainerEl: HTMLElement
+
+  private model: Model
+  private selector: CheckboxMultiSelect<ContributorModel>
+  private view: MonkberryView
+
+  constructor(private dash: Dash<App>) {
+    this.model = this.dash.app.model
+    this.el = this.createView()
+    this.selector = this.createMultiSelect()
+  }
+
+  public show() {
+    document.body.appendChild(this.el)
+    this.el.showModal()
+  }
+
+  public selectContributors(arr: ContributorModel[]) {
+    this.selector.selectItems(arr)
+  }
+
+  public selectedContributors() {
+    return this.selector.getSelected()
+  }
+
+  private createView() {
+    this.view = render(template, document.createElement("div"))
+
+    let el = this.view.nodes[0] as HTMLDialogElement
+    this.buttonEl = el.querySelector(".js-button") as HTMLButtonElement
+    this.selectorContainerEl = el.querySelector(".js-selector-container") as HTMLElement
+
+    // By default, pressing the ESC key close the dialog. We have to prevent that.
+    el.addEventListener("cancel", ev => ev.preventDefault())
+    this.buttonEl.addEventListener("click", ev => {
+      el.close()
+      this.dash.emit("contributorSelectionDialogClosed")
+    })
+
+    return el
+  }
+
+  private createMultiSelect() {
+    let ms = this.dash.create(
+      CheckboxMultiSelect,
+      "Steps",
+      (dash: Dash, contributor: ContributorModel) => dash.create(ContributorBox, contributor)
+    )
+    this.selectorContainerEl.appendChild(ms.el)
+
+    let events = ["updateContributor", "createContributor", "deleteContributor"]
+    this.dash.listenTo<UpdateModelEvent>(this.model, events).onData(
+      data => ms.setAllItems(this.model.global.steps)
+    )
+    ms.setAllItems(this.model.global.contributors)
+
+    return ms
+  }
+}
