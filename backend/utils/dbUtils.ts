f71bdb6dc9f8c20721837eb8eab67d83754a7080
Paleo <contact@paleo.casa>
2018-03-16 08:12:18 +0100

    Upgrade mycn

--- backend/utils/dbUtils.ts
+++ backend/utils/dbUtils.ts
@@ -40,17 +40,15 @@ declare module "mycn" {
 }
 
 export async function initConnection() {
-  cn = await createDatabaseConnection(
-    sqlite3ConnectionProvider({ fileName: mainDbConf.path }),
-    {
-      init: async cn => {
-        await cn.exec("PRAGMA foreign_keys = ON")
-      },
-      poolOptions: {
-        logError: console.log
-      }
+  cn = await createDatabaseConnection({
+    provider: sqlite3ConnectionProvider({ fileName: mainDbConf.path }),
+    init: async cn => {
+      await cn.exec("PRAGMA foreign_keys = ON")
+    },
+    poolOptions: {
+      logError: console.log
     }
-  )
+  })
 
   // cn = await sqliteConnection(
   //   async () => {
@@ -65,40 +63,38 @@ export async function initConnection() {
   //   }
   // )
 
-  fileCn = await createDatabaseConnection(
-    sqlite3ConnectionProvider({ fileName: fileDbConf.path }),
-    {
-      init: async cn => {
-        await cn.exec("PRAGMA foreign_keys = ON")
-      },
-      modifyDatabaseConnection: cn => {
-        cn.prepareSqlBricks = sqlBricks => {
-          let params = sqlBricks.toParams({ placeholder: '?%d' })
-          return cn.prepare(params.text, params.values)
-        }
-        cn.execSqlBricks = sqlBricks => {
-          let params = sqlBricks.toParams({ placeholder: '?%d' })
-          return cn.exec(params.text, params.values)
-        }
-        cn.allSqlBricks = sqlBricks => {
-          let params = sqlBricks.toParams({ placeholder: '?%d' })
-          return cn.all(params.text, params.values)
-        }
-        cn.singleRowSqlBricks = sqlBricks => {
-          let params = sqlBricks.toParams({ placeholder: '?%d' })
-          return cn.singleRow(params.text, params.values)
-        }
-        cn.singleValueSqlBricks = sqlBricks => {
-          let params = sqlBricks.toParams({ placeholder: '?%d' })
-          return cn.singleValue(params.text, params.values)
-        }
-        return cn
-      },
-      poolOptions: {
-        logError: console.log
+  fileCn = await createDatabaseConnection({
+    provider: sqlite3ConnectionProvider({ fileName: fileDbConf.path }),
+    init: async cn => {
+      await cn.exec("PRAGMA foreign_keys = ON")
+    },
+    modifyDatabaseConnection: cn => {
+      cn.prepareSqlBricks = sqlBricks => {
+        let params = sqlBricks.toParams({ placeholder: '?%d' })
+        return cn.prepare(params.text, params.values)
       }
+      cn.execSqlBricks = sqlBricks => {
+        let params = sqlBricks.toParams({ placeholder: '?%d' })
+        return cn.exec(params.text, params.values)
+      }
+      cn.allSqlBricks = sqlBricks => {
+        let params = sqlBricks.toParams({ placeholder: '?%d' })
+        return cn.all(params.text, params.values)
+      }
+      cn.singleRowSqlBricks = sqlBricks => {
+        let params = sqlBricks.toParams({ placeholder: '?%d' })
+        return cn.singleRow(params.text, params.values)
+      }
+      cn.singleValueSqlBricks = sqlBricks => {
+        let params = sqlBricks.toParams({ placeholder: '?%d' })
+        return cn.singleValue(params.text, params.values)
+      }
+      return cn
+    },
+    poolOptions: {
+      logError: console.log
     }
-  )
+  })
 
   // fileCn = await sqliteConnection(async () => {
   //   let db = await open(fileDbConf.path)
