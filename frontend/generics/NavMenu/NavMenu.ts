37dd561b28a81e430fac9b24531ad24272dcf7ea
kitihounel <kitihounel@gmail.com>
2018-05-26 01:30:52 +0100

    Works on DropdownMenu

--- frontend/generics/NavMenu/NavMenu.ts
+++ frontend/generics/NavMenu/NavMenu.ts
@@ -9,9 +9,7 @@ const liTemplate = require("./li.monk")
 export type Direction = "row" | "column" | "rowReverse" | "columnReverse"
 
 export interface NavMenuOptions {
-  /**
-   * Default value is: "row"
-   */
+  /** Default value is: "row" */
   direction?: Direction
   cssClass?: string | string[]
   btnCssClass?: string | string[]
@@ -42,8 +40,7 @@ export default class NavMenu {
     for (let btn of buttons) {
       if (this.options.btnCssClass)
         btn.addCssClass(this.options.btnCssClass)
-      let view = render(liTemplate, document.createElement("div"))
-      let li = view.nodes[0] as HTMLLIElement
+      let li = render(liTemplate).rootEl() as HTMLLIElement
       li.appendChild(btn.el)
       this.el.appendChild(li)
     }
