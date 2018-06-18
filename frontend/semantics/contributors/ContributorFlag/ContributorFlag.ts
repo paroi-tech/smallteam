2452c5fd6340cfbdf9f15a9ad54ea0a7dbb4ad68
kitihounel <kitihounel@gmail.com>
2018-06-18 18:47:56 +0100

    Works on ContributorFlag

--- frontend/semantics/contributors/ContributorFlag/ContributorFlag.ts
+++ frontend/semantics/contributors/ContributorFlag/ContributorFlag.ts
@@ -1,7 +1,8 @@
 import { Model, ContributorModel, UpdateModelEvent } from "../../../AppModel/AppModel"
 import { OwnDash } from "../../../App/OwnDash"
 import { render } from "@fabtom/lt-monkberry"
-import config from "../../../../isomorphic/config"
+import { MediaModel } from "../../../AppModel/Models/MediaModel"
+import { MediaVariantFragment } from "../../../../isomorphic/meta/MediaVariant"
 
 const template = require("./ContributorFlag.monk")
 
@@ -27,6 +28,23 @@ export default class ContributorFlag {
     if (!avatar || avatar.variants.length === 0)
       this.el.textContent = this.contributor.login.charAt(0).toLocaleUpperCase()
     else
-      this.el.style.backgroundImage = `url(${avatar.variants[0].url})`
+      this.el.style.backgroundImage = `url(${this.findBestVariant(avatar).url})`
+  }
+
+  private findBestVariant(avatar: MediaModel) {
+    let choice: MediaVariantFragment | undefined = undefined
+    let minDistance = 10 ** 9
+    for (let variant of avatar.variants.filter(v => v.imgWidth && v.imgHeight)) {
+      let d = this.distanceFromIdealDim(variant.imgWidth!, variant.imgHeight!)
+      if (d < minDistance) {
+        choice = variant
+        minDistance = d
+      }
+    }
+    return choice || avatar.variants[0]
+  }
+
+  private distanceFromIdealDim(imgWidth: number, imgHeight: number) {
+    return Math.abs(48 - imgWidth) ** 2 + Math.abs(48 - imgHeight)
   }
 }
