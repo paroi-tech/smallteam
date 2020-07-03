8808979de77773451a50fa8840ffbd77fff14938
Kitihounel <kitihounel@gmail.com>
2020-07-03 19:55:34 +0100

    Updated wsEngine to use sessionID as ws identifer

--- backend/src/team/wsEngine.ts
+++ backend/src/team/wsEngine.ts
@@ -64,8 +64,8 @@ export function wsEngineInit(server: Server, sessionParser: RequestHandler) {
       if (!ws.attachedProperties)
         return
       if (!ws.attachedProperties.isAlive) {
-        const { subdomain, socketId } = ws.attachedProperties
-        unregisterWsClient(subdomain, socketId)
+        const { subdomain, sessionId } = ws.attachedProperties
+        unregisterWsClient(subdomain, sessionId)
         return ws.terminate()
       }
       ws.attachedProperties.isAlive = false
@@ -107,7 +107,7 @@ function registerWsClient(ws: WebSocketWithProperties, props: WSProperties) {
     clients = new Map<string, WebSocketWithProperties>()
     wsClients.set(props.subdomain, clients)
   }
-  clients.set(props.socketId, ws)
+  clients.set(props.sessionId, ws)
 
   ws.on("pong", function (this: WebSocketWithProperties) {
     if (!this.attachedProperties)
@@ -115,12 +115,12 @@ function registerWsClient(ws: WebSocketWithProperties, props: WSProperties) {
     this.attachedProperties.isAlive = true
   })
 
-  ws.on("close", () => unregisterWsClient(props.subdomain, props.socketId))
+  ws.on("close", () => unregisterWsClient(props.subdomain, props.sessionId))
 }
 
-function unregisterWsClient(subdomain: string, socketId: string) {
+function unregisterWsClient(subdomain: string, sessionId: string) {
   const clients = wsClients.get(subdomain)
   if (!clients)
     return
-  clients.delete(socketId)
+  clients.delete(sessionId)
 }
